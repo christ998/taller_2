722735a77afbbfae6bf99d0ad736123bd51665ea
Christian <c.herrera14@ufromail.cl>
2021-10-27 21:13:48 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="isLoaded">
+    <nav-bar/>
+    <div class="is-flex-desktop is-flex-touch is-justify-content-center my-5">
+      <span>Películas más populares</span>
+    </div>
+    <film-card-list :films="films"></film-card-list>
+    <div class="container my-4">
+      <button :class="isFetching ? 'is-loading': '' " class="button is-medium is-fullwidth is-rounded is-success"
+              @click="page++">Ver
+        más
+      </button>
+
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import NavBar from '../components/NavBar'
+import FilmCardList from '../components/FilmCardList'
+import ApiService from '../services/api.service'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    FilmCardList,
+    NavBar
+  },
+  data () {
+    return {
+      films: [],
+      page: 1,
+      isLoaded: false,
+      isFetching: false
+    }
+  },
+  methods: {
+    getPopularFilms: async function (page) {
+      const newFilms = await ApiService.getPopular(page)
+      return newFilms
+    }
+  },
+  async created () {
+    const filmsFetched = (await this.getPopularFilms(this.page)).data.results
+    this.films = this.films.concat(filmsFetched)
+    this.isLoaded = true
+  },
+
+  watch: {
+    page: async function (newValue) {
+      this.isFetching = true
+      const newFilms = await this.getPopularFilms(newValue)
+      this.films = this.films.concat(newFilms.data.results)
+      this.isFetching = false
+    }
   }
+
 }
 </script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+span {
+  font-weight: bold;
+  font-size: 26px;
+}
+</style>
